739f89bb199735caac9cb7df1eb06236102b8478
NextLvlZerO <Lars.mueller.zglp@gmail.com>
2025-04-10 07:03:59 -0500

    new Event added

---
+++ event/src/Event.java
@@ -0,0 +1,18 @@
+public class Event {
+
+    String bezeichnung;
+
+    String ort;
+    String datum;
+
+    String[] tickets;
+    public Event(String bezeichnung, String ort, String datum, String[] tickets){
+        this.bezeichnung = bezeichnung;
+        this.ort = ort;
+        this.datum = datum;
+        this.tickets = tickets;
+    }
+    public static void main(String[] args){
+
+    }
+}

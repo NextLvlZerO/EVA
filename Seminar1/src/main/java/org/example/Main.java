024148afc381cd37652301ce62fe964c85d046b0
NextLvlZerO <Lars.mueller.zglp@gmail.com>
2025-04-17 02:43:32 -0500

    updated PrimeGen

--- Seminar1/src/main/java/org/example/Main.java
+++ Seminar1/src/main/java/org/example/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args){
         Event valorantEvent = new Event(12, "valorant","Berlin", LocalDateTime.of(2025, 4, 16, 14, 30, 0),23);
         System.out.println(valorantEvent);
+        PrimeNumberGenerator primeNumberGenerator = new PrimeNumberGenerator();
+        System.out.println(primeNumberGenerator.getPrimeNumber(10,20));
     }
 }
--- Seminar1/src/main/java/org/example/PrimeNumberGenerator.java
+++ Seminar1/src/main/java/org/example/PrimeNumberGenerator.java
@@ -1,5 +1,20 @@
 package org.example;
 
 public class PrimeNumberGenerator {
-    
+    public int getPrimeNumber(int l, int r) {
+        while (l < r) {
+            if (isPrim(l)) return l;
+            l++;
+        }
+        return -1;
+    }
+
+
+    public boolean isPrim(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i < n; i++) {
+            if (n%i == 0) return false;
+        }
+        return true;
+    }
 }

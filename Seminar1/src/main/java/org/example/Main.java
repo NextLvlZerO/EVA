c0b78220e78006367276427eaf9deed32f733539
NextLvlZerO <Lars.mueller.zglp@gmail.com>
2025-04-17 06:50:58 -0500

    updated main

--- Seminar1/src/main/java/org/example/Main.java
+++ Seminar1/src/main/java/org/example/Main.java
@@ -5,6 +5,17 @@ public class Main {
 
     public static void main(String[] args){
         Event valorantEvent = new Event(12, "valorant","Berlin", LocalDateTime.of(2025, 4, 16, 14, 30, 0),23);
+
+        EventService eventService = new EventService();
+        try {
+            eventService.createEvent("Event1", "London", LocalDateTime.of(2025, 4, 16, 14, 30, 0),23);
+            eventService.createEvent("Event2", "London", LocalDateTime.of(2025, 4, 16, 14, 30, 0),23);
+        }
+        catch (NegativeNumberException e){
+           System.out.println(e.getMessage());
+        }
+
+
         System.out.println(valorantEvent);
         PrimeNumberGenerator primeNumberGenerator = new PrimeNumberGenerator();
         System.out.println(primeNumberGenerator.getPrimeNumber(10,20));

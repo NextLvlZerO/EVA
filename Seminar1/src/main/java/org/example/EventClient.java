964df51f82bdf7a5b3191b8d390a88ac1c6f5ad9
Julian <cy21ujid@studserv.uni-leipzig.de>
2025-04-17 14:21:51 +0200

    lol

--- Seminar1/src/main/java/org/example/EventClient.java
+++ Seminar1/src/main/java/org/example/EventClient.java
@@ -18,8 +18,8 @@ public EventClient() {
         System.out.println("Creating a new event");
         System.out.println("Please enter the data of the event you would like to create");
         System.out.println("Pattern: name, location, date, tickets");
-        String eingabe = sc.nextLine();
-        String[] data = eingabe.split(", ");
+        String input = sc.nextLine();
+        String[] data = input.split(", ");
 
         try {
             eventService.createEvent(data[0], data[1], LocalDateTime.parse(data[2]), Integer.parseInt(data[3]));
@@ -31,15 +31,27 @@ public EventClient() {
         sc.close();
     }
 
-    public void deleteEvent() {}
+    public void deleteEvent() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Deleting a event");
+    System.out.println("Please enter the id of the event you would like to delete");
+    String input = sc.nextLine();
+    eventService.deleteEvent(Integer.parseInt(input));
+    }
 
     public void readEvent() {}
 
     public void updateEvent() {}
 
-    public void readAllEvents() {}
+    public void readAllEvents() {
+    System.out.println("Reading all the events");
+    eventService.getAllEvents();
+    }
 
-    public void deleteAllEvents() {}
+    public void deleteAllEvents() {
+    System.out.println("Deleting all the events");
+    eventService.deleteAllEvents();
+    }
 
 
 

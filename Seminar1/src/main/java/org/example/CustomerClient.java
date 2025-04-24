066020799c04f27a8273e9d5c0a31290ace36a0d
NextLvlZerO <Lars.mueller.zglp@gmail.com>
2025-04-24 06:50:15 -0500

    added switch stuff

--- Seminar1/src/main/java/org/example/CustomerClient.java
+++ Seminar1/src/main/java/org/example/CustomerClient.java
@@ -12,22 +12,33 @@ public class CustomerClient {
     }
 
     public void customerManagement(Scanner scanner){
-        System.out.println("What would you like to do?");
-        System.out.println("1 - Create a new customer");
-        System.out.println("2 - Get a customer");
-        System.out.println("3 - Update a customer");
-        System.out.println("4 - Delete a customer");
-        System.out.println("5 - Get all customers");
-        System.out.println("6 - Delete all customers");
-        System.out.println("7 - Exit");
+        while (true) {
+            System.out.println("What would you like to do?");
+            System.out.println("1 - Create a new customer");
+            System.out.println("2 - Get a customer");
+            System.out.println("3 - Update a customer");
+            System.out.println("4 - Delete a customer");
+            System.out.println("5 - Get all customers");
+            System.out.println("6 - Delete all customers");
+            System.out.println("7 - Exit");
 
-        String command = scanner.nextLine();
+            String command = scanner.nextLine();
 
-        switch(command){
-            case "1":createCustomer(scanner);
-                break;
-
-
+            switch(command){
+                case "1":createCustomer(scanner);
+                    break;
+                case "2":getCustomer(scanner);
+                    break;
+                case "3":updateCustomer(scanner);
+                    break;
+                case "4":deleteCustomer(scanner);
+                    break;
+                case "5":getAllCustomers();
+                    break;
+                case "6":deleteAllCustomers();
+                    break;
+                case "7": return;
+            }
         }
     }
 
@@ -56,6 +67,31 @@ public class CustomerClient {
         System.out.println("Please enter the id of the event you would like to delete");
         String input = scanner.nextLine();
         customerService.deleteCustomer(Integer.parseInt(input));
+    }
+
+    public void getCustomer(Scanner scanner){
+        System.out.println("Reading a customer");
+        System.out.println("Please enter the id of the event you would like to read");
+        String input = scanner.nextLine();
+        customerService.readCustomer(Integer.parseInt(input));
+    }
+
+    public void updateCustomer(Scanner scanner){
+        System.out.println("Updating a customer");
+        System.out.println("Please enter the data of the event you would like to update");
+        System.out.println("Pattern:id, username, email, birthday");
+        String input = scanner.nextLine();
+        customerService.updateCustomer(Integer.parseInt(input.split(",")[0]), input.split(",")[1], input.split(",")[2], LocalDate.parse(input.split(",")[3]));
+    }
+
+    public void getAllCustomers(){
+        System.out.println("Reading all the customers");
+        customerService.printAllCustomers();
+    }
+
+    public void deleteAllCustomers(){
+        System.out.println("Deleting all the customers");
+        customerService.deleteAllCustomers();
     }
 
 

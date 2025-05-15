dd95c03540355019caacf07543c1b5fb7c7c9e4c
NextLvlZerO <Lars.mueller.zglp@gmail.com>
2025-05-15 05:34:42 -0500

    new htings

--- Seminar1/src/main/java/Client/PerformanceClient.java
+++ Seminar1/src/main/java/Client/PerformanceClient.java
@@ -1,5 +1,8 @@
 package Client;
 
+import Interfaces.CustomerServiceInterface;
+import Interfaces.EventServiceInterface;
+import Interfaces.TicketServiceInterface;
 import Main.TicketShop;
 import Models.Customer;
 import Models.Event;
@@ -7,14 +10,21 @@ import Models.Event;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PerformanceClient {
 
     TicketShop ticketShop;
+    EventServiceInterface eventService;
+    CustomerServiceInterface customerService;
+    TicketServiceInterface ticketService;
 
     public PerformanceClient(TicketShop ticketShop) {
         this.ticketShop = ticketShop;
+        this.eventService = ticketShop.getEventService();
+        this.customerService = ticketShop.getCustomerService();
+        this.ticketService = ticketShop.getTicketService();
     }
 
     public void stressTest() {
@@ -23,7 +33,7 @@ public class PerformanceClient {
 
         for (int i = 0; i < 100; i++) {
             try {
-                ticketShop.getEventService().createEvent("test", "fooo", LocalDateTime.of(LocalDate.of(2026,10,10), LocalTime.of(10,10)),1000);
+                eventService.createEvent("test", "fooo", LocalDateTime.of(LocalDate.of(2026,10,10), LocalTime.of(10,10)),2000);
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
@@ -31,18 +41,17 @@ public class PerformanceClient {
 
         for (int i = 0; i < 1000; i++) {
             try {
-                ticketShop.getCustomerService().createCustomer("Oskar suxx", "foo@web.de", LocalDate.of(2002, 10, 10));
+                customerService.createCustomer("Oskar suxx", "foo@web.de", LocalDate.of(2002, 10, 10));
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
 
-        List<Event> events1 = ticketShop.getEventService().getAllEvents();
-
-        for (Customer c : ticketShop.getCustomerService().getAllCustomer()) {
-            for (Event e : ticketShop.getEventService().getAllEvents()) {
+        for (Customer c : customerService.getAllCustomer()) {
+            for (Event e : eventService.getAllEvents()) {
                 try {
-                    ticketShop.getTicketService().createTicket(c.getId(),e.getId());
+                    ticketService.createTicket(c.getId(),e.getId());
+                    System.out.println("created Ticket");
                 } catch (Exception ee) {
                     System.out.println(ee.getMessage());
                 }
@@ -50,31 +59,31 @@ public class PerformanceClient {
             }
 
         }
+
+        List<Event> events2 = new ArrayList<>();
 
         for (int i = 0; i < 100; i++) {
             try {
-                ticketShop.getEventService().createEvent("test2", "fooo", LocalDateTime.of(LocalDate.of(2026,10,10), LocalTime.of(10,10)),1000);
+                events2.add(eventService.createEvent("test2", "fooo", LocalDateTime.of(LocalDate.of(2026,10,10), LocalTime.of(10,10)),4000));
             } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
 
-        List<Event> events2 = ticketShop.getEventService().getAllEvents();
-        events2.removeAll(events1);
-
-        for (Customer c : ticketShop.getCustomerService().getAllCustomer()) {
+        for (Customer c : customerService.getAllCustomer()) {
             for (Event e : events2) {
                 try {
-                    ticketShop.getTicketService().createTicket(c.getId(),e.getId());
+                    ticketService.createTicket(c.getId(),e.getId());
+                    ticketService.createTicket(c.getId(),e.getId());
                 } catch (Exception ee) {
                     System.out.println(ee.getMessage());
                 }
 
             }
-
         }
 
         System.out.println("Finished test");
+        eventService.printAllEvents();
 
     }
 
--- Seminar1/src/main/java/Interfaces/EventServiceInterface.java
+++ Seminar1/src/main/java/Interfaces/EventServiceInterface.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface EventServiceInterface {
-    void createEvent(String bezeichnung, String ort, LocalDateTime datum, int tickets) throws NegativeNumberException, InvalidDateException;
+    Event createEvent(String bezeichnung, String ort, LocalDateTime datum, int tickets) throws NegativeNumberException, InvalidDateException;
 
     Event getEvent(int id);
 

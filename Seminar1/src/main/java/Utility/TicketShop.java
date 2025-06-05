ea51463cd24a140f8bac86705235418b357907d1
Julian <cy21ujid@studserv.uni-leipzig.de>
2025-06-05 13:48:42 +0200

    >.>

--- Seminar1/src/main/java/Utility/TicketShop.java
+++ Seminar1/src/main/java/Utility/TicketShop.java
@@ -9,6 +9,9 @@ import Services.EventService;
 import Services.LogService;
 import Services.TicketService;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
 public class TicketShop {
 
 
@@ -16,6 +19,14 @@ public class TicketShop {
     private final CustomerService customerService;
     private final TicketService ticketService;
     private final LogService logService;
+
+    public enum type {
+        EVENT, CUSTOMER, TICKET
+    }
+
+    public enum method {
+        CREATE, UPDATE, DELETE
+    }
 
     public TicketShop() {
         this.eventService = new EventService();
@@ -39,4 +50,49 @@ public class TicketShop {
 
     public LogServiceInterface getLogService() { return logService; }
 
+    public void convertString(String input) {
+        String[] tokens = input.split(",");
+        tokens = Arrays.stream(tokens).map(String::trim).toArray(String[]::new);
+        String sType = tokens[0].toUpperCase();
+        String method = tokens[1];
+        String[] params = Arrays.copyOfRange(tokens, 2, tokens.length);
+        type eType = type.valueOf(sType);
+        switch (eType) {
+            case EVENT:
+                cónvertStringEvent(method, params);
+                break;
+            case CUSTOMER:
+                break;
+            case TICKET:
+                break;
+            default:
+
+        }
+    }
+
+    private void cónvertStringEvent(String sMethod, String[] params) {
+        method eMethod = method.valueOf(sMethod.toUpperCase());
+        switch (eMethod) {
+            case CREATE:
+                try {
+                    if (params.length != 4) {
+                        throw new IllegalArgumentException("Wrong number of parameters");
+                    }
+                    String bezeichnung = params[0];
+                    String ort = params[1];
+                    LocalDateTime date = LocalDateTime.parse(params[2]);
+                    int tickets = Integer.parseInt(params[3]);
+                    eventService.createEvent(bezeichnung, ort, date, tickets);
+                } catch (Exception e) {
+                    logService.error(e.getMessage());
+                }
+                break;
+            case UPDATE:
+                break;
+            case DELETE:
+                break;
+            default:
+                throw new IllegalArgumentException("Wrong method");
+        }
+    }
 }
